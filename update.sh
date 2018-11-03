3ffb1ce63ccfa3f7bdcb9cf343fa9bb4a4ddd9f9
Michael Neu <git@michaeln.eu>
2018-11-03 23:43:32 +0100

    specify source branch in update script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 export LC_ALL=en_US
 
-git_update=`git pull`
+git_update=`git pull origin master`
 rocket_update=`docker pull rocketchat/rocket.chat:latest`
 
 if [[ $git_update =~ "Already" || $rocket_update =~ "Image is up to date" ]]; then
