40ccdf56a7dba3080aee2ea0af0d20b0d37c4cfc
Michael Neu <git@michaeln.eu>
2018-11-03 23:05:30 +0100

    add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LC_ALL=en_US
+
+git_update=`git pull`
+rocket_update=`docker pull rocketchat/rocket.chat:latest`
+
+if [[ $git_update =~ "Already" || $rocket_update =~ "Image is up to date" ]]; then
+  echo "already up to date"
+else
+  echo "restarting server"
+  docker-compose up -d
+fi
